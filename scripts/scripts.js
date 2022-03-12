0e10975dfc8620c8a39d70e6f918c152ae5c9256
88parham <93513876+88parham@users.noreply.github.com>
2022-03-12 17:33:17 +0330

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,101 @@
+let total_price = 120000;
+let subscription_price = 108000;
+let charge_price = 12000;
+
+let total_price_s = 0;
+let subscription_price_s = 00;
+let charge_price_s = 000;
+
+document.getElementById("total-price").innerHTML = total_price;
+document.getElementById("subscription-price").innerHTML = subscription_price;
+document.getElementById("charge-price").innerHTML = charge_price;
+
+function tp1() {
+  total_price = total_price;
+  return total_price;
+}
+
+function sp1() {
+  subscription_price = subscription_price;
+  return subscription_price;
+}
+
+function cp1() {
+  charge_price = charge_price;
+  return charge_price;
+}
+
+function tp3() {
+  total_price = total_price * 3;
+  return total_price;
+}
+
+function sp3() {
+  subscription_price = subscription_price * 3;
+  return subscription_price;
+}
+
+function cp3() {
+  charge_price = charge_price * 3;
+  return charge_price;
+}
+
+function tp6() {
+  total_price = total_price * 6;
+  return total_price;
+}
+
+function sp6() {
+  subscription_price = subscription_price * 6;
+  return subscription_price;
+}
+
+function cp6() {
+  charge_price = charge_price * 6;
+  return charge_price;
+}
+
+function tp9() {
+  total_price = total_price * 9;
+  return total_price;
+}
+
+function sp9() {
+  subscription_price = subscription_price * 9;
+  return subscription_price;
+}
+
+function cp9() {
+  charge_price = charge_price * 9;
+  return charge_price;
+}
+
+function tp12() {
+  total_price = total_price * 12;
+  return total_price;
+}
+
+function sp12() {
+  subscription_price = subscription_price * 12;
+  return subscription_price;
+}
+
+function cp12() {
+  charge_price = charge_price * 12;
+  return charge_price;
+}
+
+function tp12school() {
+  total-price = total_price_s;
+  return total_price_s;
+}
+
+function sp12school() {
+  subscription-price = subscription_price_s;
+  return subscription_price_s;
+}
+
+function cp12school() {
+  charge-price = charge_price_s;
+  return charge_price_s;
+}
